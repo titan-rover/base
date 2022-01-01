c2fc5d26ed91e943e73faa8abef395fa12d7c38a
KhangPham9 <khangpham9999@csu.fullerton.edu>
2022-01-01 13:32:05 -0800

    Overlayed the Map Lock button on the map

--- ui/web-ui/src/components/MapTile.js
+++ ui/web-ui/src/components/MapTile.js
@@ -1,25 +1,33 @@
-import React, { useEffect, useState, useRef  } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
-import { MapContainer, Marker, Popup, TileLayer, useMap  } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMap  } from 'react-leaflet';
 import '../css/MapTile.css';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import {divIcon} from "leaflet";
-import localforage from 'localforage';
 import icon from 'leaflet/dist/images/marker-icon.png';
 
 
 /*makes the map to center on the marker*/
 function ChangeMapView({ coords }) {
     const map = useMap();
-
-
     map.setView(coords, map.getZoom());
-
     return null;
-
-
 }
+
+
+/*custom control displayed on leaflet map*/
+function ButtonControl({click}){
+  return (
+    <div className={'leaflet-bottom leaflet-left'}>
+      <div className="leaflet-control leaflet-bar">
+        {/*the button decides whether the map is locked on to the rover marker or not*/}
+        <button onClick={click} className="lock-button" id="button">Unlock</button>
+      </div>
+    </div>
+  )
+}
+
+/*returns the marker that represents the rover*/
 function RotateMarker(props)
 {
   var customIcon = new L.divIcon({
@@ -34,96 +42,71 @@ function RotateMarker(props)
 }
 
 
-/*Defines the default marker*/
+/*Creating the default marker*/
 const DefaultIcon = L.icon({
     iconUrl: icon,
 });
 L.Marker.prototype.options.icon = DefaultIcon;
 
-
-
-
+/* function that creates the map*/
 function MapTile(props)
 {
 
   const [lat, setLat] = useState(props.latitude);
   const [lng, setLng] = useState(props.longitude);
-  const [toggled, setToggle] = useState(true);
+  const [locked, setLock] = useState(true);
   const [markerList, setMarkerList] = useState(props.markerList);
   const [angle, setAngle] = useState(props.angle);
 
-/*runs when the MapTile component is mounted and unmounted */
-  useEffect(() =>{
 
-
-    return () => {
-
-    }
-
-  }, [])
-
-
+/*updates the angle */
   useEffect(() => {
     setAngle(props.angle);
-
   },[props.angle]
 )
 
-/*checks changes the latitude and longitude when they are changed in App.js*/
+/*updates the latitude and longitude */
   useEffect(() => {
     setLat(props.latitude);
     setLng(props.longitude);
   }, [props.latitude, props.longitude]
 )
 
+/*updates the marker list */
   useEffect(() => {
     setMarkerList(props.markerList);
   }, [props.markerList],
 )
 
-const mounted = useRef();
-useEffect(() => {
-  if (!mounted.current) {
-    // do componentDidMount logic
-
-    mounted.current = true;
-  } else {
-    // do componentDidUpdate logic
-
-
-  }
-});
-
-
 
 /*function that locks and unlocks the map on the centner*/
   const onClickHandler = (e) => {
-    if(toggled)
+    let btn = document.getElementById("button");
+    if(locked)
     {
-      setToggle(false);
+      setLock(false);
+      btn.innerHTML = 'Lock';
     }
     else
     {
-      setToggle(true);
+      setLock(true);
+      btn.innerHTML = 'Unlock';
     }
   }
+
 
   return (
     <Card className="text-center" id="map-id">
       <Card.Header>Map Tile</Card.Header>
       <Card.Body>
-        <button onClick={onClickHandler}>toggle</button>
-        <MapContainer center={[lat, lng]} zoom={15} maxZoom={18} scrollWheelZoom={false}>
-          {( ()=> {
-            if(toggled === true)
+
+        <MapContainer center={[lat, lng]} zoom={15} maxZoom={15} scrollWheelZoom={false} doubleClickZoom={false}>
+          {(() => {
+            if(locked === true)
             {
               return(
               <ChangeMapView coords ={[lat, lng]} />
               )
-            }
-            else
-            {
-              return (null);
             }
           })
           ()}
@@ -141,23 +124,21 @@ useEffect(() => {
 
           <RotateMarker lat={lat} lng={lng} angle={angle} />
 
+          {/*if the markerList has a length that is greater than 0 then display all of the markers in the list*/}
           {
                 markerList.length > 0 &&
                 markerList.map((marker, index) => {
 
                  return (
-                   <Marker key={index} position={[markerList[index].latitude, markerList[index].longitude]}  >
+                   <Marker key={index} position={[marker.latitude, marker.longitude]}  >
 
                    </Marker>
 
                     );
                   })
 
-
-
-
           }
-
+          <ButtonControl click={onClickHandler}/>
         </MapContainer>
       </Card.Body>
     </Card>
@@ -170,11 +151,12 @@ useEffect(() => {
 export default MapTile;
 /*
 code for testing purposes
+
 componentDidMount() {
 
   this.timerID = setInterval(
     () => this.tick(this.state),
-    1000
+    500
   );
 
 }
@@ -183,8 +165,9 @@ tick(state)
 {
 
   this.setState(() => ({
-    lat: this.state.lat + 0.00001,
-    lng: this.state.lng + 0.00001
+    angle: this.state.angle + 5,
+    latitude: this.state.latitude + 0.0001,
+    longitude: this.state.longitude + 0.0001
   }))
 
 }
@@ -194,97 +177,6 @@ componentDidUpdate(prevProps, prevState)
 
 }
 
+url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
 
-*/
-/* MapTile with class
-class MapTile extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      lat: this.props.coordinates[0],
-      lng: this.props.coordinates[1],
-      zoom: 15,
-      maxZoom: 18
-    }
-
-  }
-
-  componentDidMount() {
-    //Defining the offline layer for the map
-    const map = L.map('map-id');
-    const offlineLayer = L.tileLayer.offline('https://{s}.tile.osm.org/{z}/{x}/{y}.png', localforage, {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      subdomains: 'abc',
-      minZoom: 13,
-      maxZoom: 19,
-      crossOrigin: true
-    });
-    offlineLayer.addTo(map);//add the offline layer
-    map.zoomControl.remove();
-    //  map.addControl(searchControl);
-    /*
-    this.timerID = setInterval(
-      () => this.tick(this.state),
-      100
-    );
-
-  }
-  /*
-  tick(state)
-  {
-
-    this.setState((state) => ({
-      lat: this.state.lat,
-      lng: this.state.lng,
-    }))
-
-  }
-
-
-  shouldComponentUpdate(nextProps) {
-    return true;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if(prevProps.coordinates !== this.props.coordinates)
-    {
-      this.setState( () => ({
-
-        lat: this.props.coordinates[0],
-        lng: this.props.coordinates[1],
-
-      }))
-      //console.log('hello')
-    }
-  }
-
-
-  //render the map
-  render() {
-    var position = [this.state.lat, this.state.lng];
-    return (
-      <Card className="text-center" id="map-id">
-      <Card.Header>Map Tile</Card.Header>
-      <Card.Body>
-      <button onClick={handlesetview} >test </button>
-      <MapContainer center={position} zoom={this.state.zoom} maxZoom={this.state.maxZoom} scrollWheelZoom={false}>
-      <ChangeMapView coords ={position} />
-      <TileLayer
-      /*
-      url to      url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-      another map attribution= 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-
-      attribution="&copy; <a href=&quot;https://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-
-      </Marker>
-
-      </MapContainer>
-      </Card.Body>
-      </Card>
-    );
-  }
-}
 */
